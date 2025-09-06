b75adf9b562cca09a4a61a6a8e40e40135bac8b2
BhargaviSV <bhargavisv04@gmail.com>
2025-09-06 13:01:15 +0530

    Update project.v
    initial design commit for mux_2x1

--- src/project.v
+++ src/project.v
@@ -17,11 +17,17 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    assign s0=ui_in[0];
+    assign s0=ui_in[1];
+    assign s0=ui_in[2];
+    
+    assign y0=s0?i1:i0;
+    
+    assign uo_out[0]=y0;
+    assign uo_out[7:1]=7'b0;
+    assign uio_out=8'b0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena, clk, rst_n, 1'b0, ui_in[7:3], uio_in[7:0]};
 
 endmodule
